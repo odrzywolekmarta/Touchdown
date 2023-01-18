fbd9c787a2e688e87ee831df544759cffe886bf6
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-18 19:55:08 +0100

    add footer view

---
+++ Touchdown/Touchdown/Utility/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Touchdown
+//
+//  Created by Majka on 18/01/2023.
+//
+
+import SwiftUI
+
+//COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+
