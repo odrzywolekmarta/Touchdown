523bb8987a1d72ad79c242291208c2598b585ff9
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-20 16:34:12 +0100

    add bundle extension

---
+++ Touchdown/Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Majka on 20/01/2023.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
